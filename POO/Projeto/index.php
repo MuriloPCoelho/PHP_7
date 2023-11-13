f820ce7d16efd52503e7df7bd25997b9f82c159d
Murilo Pereira Coelho <90260387+MuriloPCoelho@users.noreply.github.com>
2023-11-13 17:22:38 -0300

    Adicionando aulas

--- POO/Projeto/index.php
+++ POO/Projeto/index.php
@@ -6,6 +6,15 @@ $primeiraConta->cpfTitular = '091829402';
 $primeiraConta->nomeTitular = 'Mauricio Saraiva';
 $primeiraConta->saldo = 25000;
 
+$primeiraConta->sacar(150);
+
+
+$segundaConta = new Conta();
+$segundaConta->saldo = 10000;
+
+$primeiraConta->tranferir(625, $segundaConta);
+
 echo '<pre>';
 echo var_dump($primeiraConta);
+echo var_dump($segundaConta);
 echo '</pre>';
--- POO/Projeto/src/Conta.php
+++ POO/Projeto/src/Conta.php
@@ -3,7 +3,47 @@
 
 class Conta 
 {
+  // Atributos
   public $cpfTitular;
-  public $saldo;
+  public $saldo = 0; //define um valor inicial
   public $nomeTitular;
+
+
+  // Métodos
+  public function sacar(float $valorDoSaque) : void
+  {
+    if ($valorDoSaque > $this->saldo) {
+      echo "Saldo indisponível";
+
+    } else {
+      $this->saldo -= $valorDoSaque;
+
+    }
+  }
+
+  public function depositar(float $valorDoDeposito) : void 
+  {
+    if ($valorDoDeposito < 0) {
+      echo "Valor precisa ser positivo";
+
+    } else {
+      $this->saldo += $valorDoDeposito;
+
+    }
+
+  }
+
+  public function tranferir(float $valorDaTransferencia, Conta $contaDeDestino): void
+  {
+    if ($valorDaTransferencia > $this->saldo) {
+      echo "Saldo Indisponível";
+      
+    } else {
+      $this->sacar($valorDaTransferencia);
+      $contaDeDestino->depositar($valorDaTransferencia);
+
+    }
+
+  }
+
 }
