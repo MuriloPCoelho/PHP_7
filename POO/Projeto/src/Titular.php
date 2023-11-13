0cdf29e368b9cf8606878852f883cf26d8df716d
Murilo.coelho <murilo.coelho@ufcspa1.onmicrosoft.com>
2023-11-13 13:58:33 -0300

    Adicionado as aulas do curso de POO em PHP

---
+++ POO/Projeto/src/Titular.php
@@ -0,0 +1,33 @@
+<?php
+
+class Titular {
+  private $nome;
+  private $cpf;
+
+  public function __construct(string $nome, string $cpf) 
+  {
+    $this->nome = $nome;
+    $this->cpf = $cpf;
+
+  }
+
+  // public function setNome(string $nome) : void
+  // {
+  //   $this->nomeTitular = $nome;
+  // }
+
+  public function getNome() : string
+  {
+    return $this->nome;
+  }
+
+  // public function setCPF(string $cpf) : void
+  // {
+  //   $this->cpfTitular = $cpf;
+  // }
+
+  public function getCPF() : string
+  {
+    return $this->cpf;
+  }
+}
